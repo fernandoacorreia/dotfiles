356b0ed016ba93d2e0a53bcf0c128aed46779f77
Fernando Correia <fernandoacorreia@gmail.com>
2021-04-19 17:46:59 -0700

    Fix Homebrew path

--- shell/profile.sh
+++ shell/profile.sh
@@ -1,6 +1,6 @@
 # Homebrew paths
-if [ "${HOMEBREW_PREFIX:-}" != "" ]; then
-  eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
+if [ -x "/opt/homebrew/bin/brew" ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Import secrets
