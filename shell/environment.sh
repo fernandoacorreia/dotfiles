cf208c299de32c61771f81a1c9b0243528e93ae6
Fernando Correia <fernando.correia@snorkel.ai>
2024-11-06 11:58:55 -0800

    nvm bash completion

--- shell/environment.sh
+++ shell/environment.sh
@@ -64,4 +64,5 @@ export SDKMAN_DIR="$HOME/.sdkman"
 
 # nvm - see https://github.com/nvm-sh/nvm?tab=readme-ov-file#install--update-script
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
