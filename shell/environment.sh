839fc650ebcfc3c3d9003635c7cf5d48a6febc8f
Fernando Correia <fernandoacorreia@gmail.com>
2025-11-12 09:14:37 -0800

    Move NVM_DIR & SDKMAN_DIR to zshrc

--- shell/environment.sh
+++ shell/environment.sh
@@ -33,13 +33,3 @@ fi
 if [ -f $HOME/.linuxbrew/bin/brew ]; then
   eval $($HOME/.linuxbrew/bin/brew shellenv)
 fi
-
-# SDKMAN! -- See https://sdkman.io/usage
-export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-
-# nvm - see https://github.com/nvm-sh/nvm?tab=readme-ov-file#install--update-script
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
